bd34847e776d15d9d66893b9836fb4a02aa1e61e
duxin <duxins@gmail.com>
2014-10-29 16:12:06 +0800

    New look for the main view

--- tixing/Classes/Views/DXNotificationCell.m
+++ tixing/Classes/Views/DXNotificationCell.m
@@ -8,12 +8,17 @@
 
 #import "DXNotificationCell.h"
 
+@interface DXNotificationCell()
+@property (nonatomic, weak) IBOutlet UIView *ballonView;
+@end
+
 @implementation DXNotificationCell
 
 - (void)awakeFromNib {
   CALayer *layer = self.thumbImageView.layer;
   layer.cornerRadius = 4;
   layer.masksToBounds = YES;
+  [self updateBallonViewBackground];
 }
 
 
@@ -23,6 +28,25 @@
   [self.contentView setNeedsLayout];
   [self.contentView layoutIfNeeded];
   self.messageLabel.preferredMaxLayoutWidth = CGRectGetWidth(self.messageLabel.frame);
+}
+
+- (void)updateBallonViewBackground
+{
+  self.ballonView.backgroundColor = [UIColor colorWithRed:0.94f green:0.94f blue:0.95f alpha:1];
+}
+
+- (void)setHighlighted:(BOOL)highlighted animated:(BOOL)animated {
+  [super setHighlighted:highlighted animated:animated];
+  if (highlighted) {
+    [self updateBallonViewBackground];
+  }
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated{
+  [super setSelected:selected animated:animated];
+  if (selected) {
+    [self updateBallonViewBackground];
+  }
 }
 
 @end
