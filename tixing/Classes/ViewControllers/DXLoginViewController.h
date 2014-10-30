757fea68d877bc1a678921d4c66a32e1c2fdf42f
duxin <duxins@gmail.com>
2014-10-30 22:10:34 +0800

    Change `LoginViewController`'s superview to `UITableViewController`

--- tixing/Classes/ViewControllers/DXLoginViewController.h
+++ tixing/Classes/ViewControllers/DXLoginViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface DXLoginViewController : UIViewController
+@interface DXLoginViewController : UITableViewController
 
 @end
--- tixing/Classes/ViewControllers/DXLoginViewController.m
+++ tixing/Classes/ViewControllers/DXLoginViewController.m
@@ -10,15 +10,12 @@
 #import "DXCredentialStore.h"
 #import "DXAPIClient.h"
 
-@interface DXLoginViewController () <UITableViewDataSource, UITableViewDelegate>
-@property (weak, nonatomic) IBOutlet UITextField *emailField;
+@interface DXLoginViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *nameField;
 @property (weak, nonatomic) IBOutlet UITextField *passwordField;
-@property (weak, nonatomic) IBOutlet UITableView *tableView;
 @end
 
-@implementation DXLoginViewController{
-  BOOL justLaunched;
-}
+@implementation DXLoginViewController
 
 #pragma mark -
 #pragma mark Lifecycle
@@ -26,29 +23,17 @@
 - (void)viewDidLoad {
   [super viewDidLoad];
   self.title = @"登录";
-  justLaunched = YES;
-  [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(didLogOut) name:TixingNotificationLogout object:nil];
 }
 
 - (void)viewDidAppear:(BOOL)animated
 {
   [super viewDidAppear:animated];
   
-  if (justLaunched) {
-    if ([DXCredentialStore sharedStore].isLoggedIn) {
-      [self didLogInAnimated:NO];
-    }
-    justLaunched = NO;
-  }
-}
-
-- (void)dealloc
-{
-  [[NSNotificationCenter defaultCenter] removeObserver:self];
+  [self.nameField becomeFirstResponder];
 }
 
 - (IBAction)loginButtonPressed:(id)sender {
-  NSString *name = self.emailField.text;
+  NSString *name = self.nameField.text;
   NSString *password = self.passwordField.text;
   
   [[[DXAPIClient sharedClient] loginWithName:name password:password] subscribeNext:^(id x) {
@@ -75,21 +60,5 @@
 {
   return YES;
 }
-
-#pragma mark -
-#pragma mark UITableViewDataSource
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
-{
-  return 1;
-}
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
-  return 2;
-}
-
-#pragma mark -
-#pragma mark UITableViewDelegate
-
 
 @end
