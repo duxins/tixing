aa9bd1ad5a62a1e1a639e1c29722b6d9783d7744
duxin <duxins@gmail.com>
2014-12-16 00:49:09 +0800

    Add support for sharing

--- tixing/Classes/ViewControllers/DXNotificationViewController.m
+++ tixing/Classes/ViewControllers/DXNotificationViewController.m
@@ -13,13 +13,18 @@
 #import "DXWebViewController.h"
 #import "DXNavigationViewController.h"
 
-@interface DXNotificationViewController ()
+@import MessageUI;
+
+@interface DXNotificationViewController () <MFMailComposeViewControllerDelegate, UINavigationControllerDelegate>
 @property (nonatomic, weak) IBOutlet UILabel *titleLabel;
 @property (nonatomic, weak) IBOutlet UILabel *messageLabel;
 @property (nonatomic, weak) IBOutlet UIImageView *thumbImageView;
 @property (nonatomic, weak) IBOutlet UILabel *dateLabel;
 @property (nonatomic, weak) IBOutlet UIBarButtonItem *openURLBarButton;
+@property (nonatomic, weak) IBOutlet UIButton *openURLButton;
+@property (nonatomic, weak) IBOutlet UIButton *sendEmailButton;
 @property (nonatomic, assign) BOOL hasOpened;
+@property (nonatomic, strong) NSDateFormatter *dateFormatter;
 @end
 
 @implementation DXNotificationViewController
@@ -29,11 +34,11 @@
   self.hasOpened = NO;
   
   if (self.notification) {
-    NSDateFormatter *dateFormatter = [NSDateFormatter new];
-    dateFormatter.dateFormat = @"YYYY-MM-dd HH:mm:ss";
+    self.dateFormatter = [NSDateFormatter new];
+    self.dateFormatter.dateFormat = @"YYYY-MM-dd HH:mm:ss";
     
     self.titleLabel.text = self.notification.title;
-    self.dateLabel.text = [dateFormatter stringFromDate:self.notification.createdAt];
+    self.dateLabel.text = [self.dateFormatter stringFromDate:self.notification.createdAt];
     self.messageLabel.text = self.notification.message;
     self.thumbImageView.layer.cornerRadius = 4;
     self.thumbImageView.layer.masksToBounds = YES;
@@ -42,6 +47,7 @@
     UIApplication *applicatoin = [UIApplication sharedApplication];
     if (![applicatoin canOpenURL:self.notification.URL] && ![applicatoin canOpenURL:self.notification.webURL]) {
       self.openURLBarButton.enabled = NO;
+      self.openURLButton.enabled = NO;
     }
     
   }
@@ -53,7 +59,7 @@
 - (IBAction)copyButtonPressed:(id)sender
 {
   UIPasteboard *pb = [UIPasteboard generalPasteboard];
-  [pb setString:self.notification.message];
+  [pb setString:[self.notification.webURL absoluteString]];
   [DXProgressHUD showSuccessMessage:@"已复制" forView:self.view image:[UIImage imageNamed:@"clipboard"]];
 }
 
@@ -62,8 +68,35 @@
   [(DXNavigationViewController *)self.navigationController openURLforNotification:self.notification];
 }
 
+- (IBAction)sendEmailButtonPressed:(id)sender
+{
+  self.sendEmailButton.enabled = NO;
+  NSString *subject = [NSString stringWithFormat:@"来自 %@ 的提醒", self.notification.title];
+  
+  NSURL *emailTemplateURL = [[NSBundle mainBundle] URLForResource:@"share_email" withExtension:@"html"];
+  NSString *body = [NSString stringWithContentsOfURL:emailTemplateURL encoding:NSUTF8StringEncoding error:nil];
+  body = [body stringByReplacingOccurrencesOfString:@"#{TITLE}" withString:self.notification.title];
+  body = [body stringByReplacingOccurrencesOfString:@"#{MESSAGE}" withString:self.notification.message];
+  body = [body stringByReplacingOccurrencesOfString:@"#{THUMB}" withString:[self.notification.thumbURL absoluteString]];
+  body = [body stringByReplacingOccurrencesOfString:@"#{URL}" withString:[self.notification.webURL absoluteString]];
+  body = [body stringByReplacingOccurrencesOfString:@"#{DATETIME}" withString:[self.dateFormatter stringFromDate:self.notification.createdAt]];
+  MFMailComposeViewController *mc = [[MFMailComposeViewController alloc] init];
+  mc.mailComposeDelegate = self;
+  [mc setSubject:subject];
+  [mc setMessageBody:body isHTML:YES];
+  [self presentViewController:mc animated:YES completion:NULL];
+}
 
-#pragma mark - 
+#pragma mark -
+#pragma mark MFMailComposeViewControllerDelegate
+- (void) mailComposeController:(MFMailComposeViewController *)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError *)error
+{
+  NSLog(@"hello");
+  self.sendEmailButton.enabled = YES;
+  [self dismissViewControllerAnimated:YES completion:NULL];
+}
+
+#pragma mark -
 #pragma mark Navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
