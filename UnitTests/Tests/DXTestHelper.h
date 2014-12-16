d1b0a7b1a46169e5cd70031db6fee2ebe15b5bc8
duxin <duxins@gmail.com>
2014-12-16 18:11:47 +0800

    Add DXTestHelper.h

---
+++ UnitTests/Tests/DXTestHelper.h
@@ -0,0 +1,16 @@
+//
+//  DXTestHelper.h
+//  tixing
+//
+//  Created by Du Xin on 12/16/14.
+//  Copyright (c) 2014 Xin Du. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#define EXP_SHORTHAND
+#import <Expecta/Expecta.h>
+#import <Specta/Specta.h>
+#import <OCMock/OCMock.h>
+#import <OHHTTPStubs/OHHTTPStubs.h>
+#import <OHHTTPStubs/OHHTTPStubsResponse+JSON.h>
+
